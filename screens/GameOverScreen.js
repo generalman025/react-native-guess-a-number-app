41c0a05bbb14ba6a48083486c3ccc0596b000ac0
Kiattisak Chaisomboon <bass.kiattisak@gmail.com>
2021-08-13 15:05:50 +0700

    update dimension api

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
+import MainButton from "../components/MainButton";
+
+const GameOverScreen = (props) => {
+  return (
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            fadeDuration={300}
+            //   source={require("../assets/success.png")}
+            source={{
+              uri: "https://images.unsplash.com/photo-1626278664285-f796b9ee7806?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone need{" "}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{" "}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  imageContainer: {
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: Dimensions.get("window").height / 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: Dimensions.get("window").height / 40,
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+});
+
+export default GameOverScreen;
